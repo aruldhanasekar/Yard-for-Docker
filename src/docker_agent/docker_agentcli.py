7fa5103fe10f9249bcf17831c141e4cbc74e158a
Arul <aruldhanasekards@gmail.com>
2026-05-10 17:38:44 +0530

    added: New and custom help(optional arguments), and console status output in the runtime

--- src/docker_agent/docker_agentcli.py
+++ src/docker_agent/docker_agentcli.py
@@ -1,7 +1,10 @@
 import asyncio
 import json
-
-from typer import Typer
+from typing import Optional, List
+import typer
+from typer import Typer, Context, Option, Argument
+from docker_agent.console.console_api import console
+from docker_agent.console.help_oargs import custom_help
 from docker_agent.helper.intent_agent import intent_identifier
 
 
@@ -9,27 +12,48 @@ from docker_agent.tools.create_dockerfile import create_dockerfile
 
 
 
-app = Typer()
-
-
+app = Typer(context_settings={
+    # To prevent using the typer's default behaviour of "--help" or '-h'
+    "help_option_names" : []
+})
 
 @app.callback(invoke_without_command=True)
-def create(message: str, prod: bool = False):
-    if prod: 
-        prompt = f"{message}, and the project is intended for production use"
-
-    else:
-        prompt = f"{message}, and project is currently in development"
+def yard_agent(
+    ctx: Context,
+    prompt: str = Argument(""), 
+    help: bool = Option(
+        False,
+        "--help",
+        "-h",
+        is_eager=True,
+    ),
+):
+    if help:
+        custom_help()
+        raise typer.Exit()
     
-    # print(prompt)
+  
 
-    intent = asyncio.run(intent_identifier(prompt))
+    with console.status(
+        "[bold cyan]Analyzing user requirements..."
+    ):
+        intent = asyncio.run(intent_identifier(prompt))
 
-    intent_dict = json.loads(intent)
+    
+        intent_dict = json.loads(intent)
 
     for key, value in intent_dict.items():
         if key == "intent" and value == "create_dockerfile":
+            console.print(
+                    "[green]✓ Intent analyzed successfully[/green]"
+                )
+            console.print(
+                    "[green]Started creating Dockerfiles[/green]"
+                )
             asyncio.run(create_dockerfile(intent_dict))
             break
         else:
-            print("Currently I can create only Dockerfiles")
+            console.print(
+                "[green] I can create and write Dockerfiles [/green]"
+            )
+            break
--- src/docker_agent/helper/depfile.py
+++ src/docker_agent/helper/depfile.py
@@ -1,5 +1,6 @@
 import re
 from pathlib import Path
+from docker_agent.console.console_api import console
 
 
 def find_dep_file(start=None):
@@ -12,27 +13,53 @@ def find_dep_file(start=None):
     project_root = [current_root] + list(current_root.parents)
 
     for parent in project_root:
-        if (parent / "pyproject.toml").exists():
-            with Path.open(parent/"pyproject.toml") as f:
-                config = f.read()
+        with console.status(
+                "[bold cyan]Resolving project dependencies..."
+            ):
+            # Search for pyproject.toml file in the root directory
+            if (parent / "pyproject.toml").exists():
 
-                match = re.search(r'requires-python\s*=\s*"((?:<=|>=|==|<|>|=)\s*([0-9.]+))"', config)
-                if match:
-                    version = match.group(1)
+                console.print(
+                    "[green]✓ Dependencies resolved successfully[/green]"
+                )
 
-                    return "pyproject.toml", version       
+                with Path.open(parent/"pyproject.toml") as f:
+                    config = f.read()
 
-        elif (parent / "requirements.txt").exists():
-            for venv_name in [".venv", "venv", "env"]:
-                cfg = parent / venv_name / "pyvenv.cfg"
-                if cfg.exists():
-                    text = cfg.read_text()
+                    with console.status(
+                        "[bold cyan]Detecting Python environment..."
+                    ):
+                        match = re.search(r'requires-python\s*=\s*"((?:<=|>=|==|<|>|=)\s*([0-9.]+))"', config)
+                        if match:
+                            version = match.group(1)
 
-                    match = re.search(r"version\s*=\s*([0-9.]+)", text)
-                    if match:
-                        version = match.group(1)
+                            console.print(
+                                f"[green]✓ Environment detected:[/green] {version}"
+                            )
 
-                        return "requirements.txt", version
+                            return "pyproject.toml", version       
+
+            # Search for requirements.txt file in the root directory
+            elif (parent / "requirements.txt").exists():
+                console.print(
+                    "[green]✓ Dependencies resolved successfully[/green]"
+                )
+                with console.status(
+                        "[bold cyan]Detecting Python environment..."
+                    ):
+                    for venv_name in [".venv", "venv", "env"]:
+                        # Searching python version in the venv
+                        cfg = parent / venv_name / "pyvenv.cfg"
+                        if cfg.exists():
+                            text = cfg.read_text()
+
+                            match = re.search(r"version\s*=\s*([0-9.]+)", text)
+                            if match:
+                                version = match.group(1)
+                                console.print(
+                                f"[green]✓ Environment detected:[/green] {version}"
+                            )
+                                return "requirements.txt", version
                 
     return None, None
         
